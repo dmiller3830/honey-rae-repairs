cb0aeb63f2399eb4d9cd3bf4a68f8c24dc3cbfe1
Daniel Miller <dmiller3830@gmail.com>
2024-05-20 15:14:59 -0400

    repository for react directory

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react"
+
+export const App = () => {
+ 
+const [count, setCount] = useState(0) // [stateVariable, setterFunction] //
+ 
+  const handleBtnClick = () => {
+   setCount(count +1)
+    console.log (count)
+  }  
+  
+  return     <>
+    <h1>Hello</h1>
+    <div>This is Amazing</div>
+    <button className="btn-secondary" onClick={handleBtnClick}>Click Me</button>
+    <div>Count: {count}</div>
+    </>
+ 
+}
